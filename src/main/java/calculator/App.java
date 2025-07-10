be8dd1930ae76f51a5ee31ae198705be724bb814
Robbie <robbie331@>
2025-07-10 14:17:45 +0900

    Level 1 - 7 ) JCF 사용하여 결과 무한 저장 및 'remove'로 첫 번째 결과 삭제 기능 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,13 +1,12 @@
 package calculator;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[10]; // 연산의 결과를 저장할 배열 생성
-        int index = 0; // 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수 선언
+        List<Integer> resultList = new ArrayList<>(); // 연산 결과가 무한으로 저장될 수 있는 리스트 생성
 
         while(true){
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -46,29 +45,35 @@ public class App {
             System.out.println("결과: " + result);
 
             // 결과 저장
-            if (index < arr.length) { // 배열에 저장된 값이 10개 미만이면
-                arr[index] = result; // 연산의 결과를 배열에 저장
-                index++; // index 증가하여 다음 index에 값이 저장될 수 있도록 함
-            } else { // 10개 초과일 경우
-                // 가장 앞 요소 제거, 한 칸씩 앞으로 밀기
-                for (int i = 0; i < arr.length - 1; i++) {
-                    arr[i] = arr[i + 1];
+            resultList.add(result);
+
+            // 저장된 결과 출력
+            System.out.print("\n현재까지의 결과: ");
+            System.out.println(resultList + "\n");
+
+            // 연산 결과 삭제 여부 확인
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String input_remove = sc.next();
+            if (input_remove.trim().equals("remove")) { // 입력값이 "remove"면 가장 먼저 저장된 연산 결과 삭제
+                if (!resultList.isEmpty()) {
+                    resultList.remove(0);
+                    System.out.println("삭제 후 현재 저장된 결과: " + resultList);
                 }
-                arr[arr.length - 1] = result; // 마지막에 가장 최근의 결과 저장
+                else { // 만약 리스트에 연산 결과가 없다면
+                    System.out.println("리스트가 비어 있습니다."); // 안내문 출력
+                }
             }
 
             // 반복 여부 확인
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String input = sc.next();
-            if (input.trim().equals("exit")) { // 입력값이 "exit"면 반복 종료
+            System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료)");
+            String input_continue = sc.next();
+            if (input_continue.trim().equals("exit")) { // 입력값이 "exit"면 반복 종료
                 break;
             }
         }
 
-        // 저장된 배열 출력
-        System.out.print("배열 저장 결과: ");
-        for (int i = 0; i < index; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        // 저장된 결과 출력
+        System.out.print("리스트 저장 결과: ");
+        System.out.println(resultList);
     }
 }
