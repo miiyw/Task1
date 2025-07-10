bf36b7b63686f3fbb6c674cd38151a63b1ac70bc
Robbie <robbie331@>
2025-07-10 11:09:21 +0900

    Level 1 - 3 ) 입력된 연산 기호에 따라 연산 수행

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,6 +15,30 @@ public class App {
         char operator = sc.next().charAt(0); // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장
         // charAt(0): 문자열의 0번째 위치에 있는 문자(첫 글자) 반환
 
-        System.out.println("입력된 기호: " + operator);
+        int result = 0; // 결과 저장용 변수 초기화
+        // 입력된 연산 기호에 따라 연산 수행
+        switch (operator) {
+            case '+': // 덧셈 연산
+                result = num1 + num2;
+                break;
+            case '-': // 뺄셈 연산
+                result = num1 - num2;
+                break;
+            case '*': // 곱셈 연산
+                result = num1 * num2;
+                break;
+            case '/': // 나눗셈 연산
+                if (num2 == 0) { // 나눗셈에서 0으로 나누는 경우 예외 처리
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                    return; // 잘못된 입력이기 때문에 프로그램 종료
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("잘못된 입력입니다."); // 정의되지 않은 연산 기호 입력 시
+                return; // 프로그램 종료
+        }
+
+        System.out.println("결과: " + result);
     }
 }
