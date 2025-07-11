4d5af2b70f1bd4c7c9a053d21f376a2ee592e8bd
Robbie <robbie331@>
2025-07-11 14:38:42 +0900

    Level 2 - 7 ) 원의 넓이를 계산하여 반환하는 메서드 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,71 +7,101 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         // Calculator 인스턴스 생성
-        List<Integer> paramList = new ArrayList<>();
-        Calculator calc = new Calculator(paramList);
+        Calculator calc = new Calculator();
 
-        while(true){
-            try {
-                int num1;
+        while(true) {
+            System.out.println("1. 사칙 연산\n2. 원의 넓이 계산");
+            System.out.print("메뉴를 선택하세요: ");
+            int input = sc.nextInt();
+            if (input == 1) {
+                try {
+                    int num1;
 
-                while (true) {
-                    System.out.print("첫 번째 숫자를 입력하세요: ");
-                    num1 = sc.nextInt();
-                    if (num1 >= 0) break; // 양수만 입력받기 위한 조건
-                    System.out.println("음수는 입력할 수 없습니다.");
+                    while (true) {
+                        System.out.print("첫 번째 숫자를 입력하세요: ");
+                        num1 = sc.nextInt();
+                        if (num1 >= 0) break; // 양수만 입력받기 위한 조건
+                        System.out.println("음수는 입력할 수 없습니다.");
+                    }
+
+                    int num2;
+                    while (true) {
+                        System.out.print("두 번째 숫자를 입력하세요: ");
+                        num2 = sc.nextInt();
+                        if (num2 >= 0) break; // 양수만 입력받기 위한 조건
+                        System.out.println("음수는 입력할 수 없습니다.");
+                    }
+
+                    System.out.print("사칙 연산 기호를 입력하세요: ");
+                    char operator = sc.next().charAt(0);
+
+                    int result = calc.calculate(num1, num2, operator);
+                    System.out.println("결과: " + result + "\n");
+
+                    // Getter 메서드 활용 - 연산 저장 결과 확인
+                    System.out.println("연산 저장 결과: " + calc.getResultList() + "\n");
+
+                } catch (DivideByZeroException | InvalidOperatorException e) { // 콘솔에 예외 처리 메시지 출력할 수 있도록 처리
+                    System.out.println("! 예외 발생: " + e.getMessage() + "\n");
+                    continue; // 예외 발생 시 처음으로 다시 입력받기
                 }
 
-                int num2;
-                while (true) {
-                    System.out.print("두 번째 숫자를 입력하세요: ");
-                    num2 = sc.nextInt();
-                    if (num2 >= 0) break; // 양수만 입력받기 위한 조건
-                    System.out.println("음수는 입력할 수 없습니다.");
+                // Setter 활용 - 리스트 초기화 여부 확인
+                System.out.println("연산 결과가 저장된 리스트를 초기화하시겠습니까? (reset 입력 시 초기화)");
+                String input_reset = sc.next();
+                if (input_reset.trim().equals("reset")) {
+                    calc.setResultList(new  ArrayList<>());
+                    System.out.println("리스트가 초기화되었습니다." + calc.getResultList() + "\n");
                 }
 
-                System.out.print("사칙 연산 기호를 입력하세요: ");
-                char operator = sc.next().charAt(0);
+                // 가장 먼저 저장된 데이터 삭제 메서드
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String input_remove = sc.next();
+                if (input_remove.trim().equals("remove")) {
+                    calc.removeResult();
+                }
 
-                int result = calc.calculate(num1, num2, operator);
-                System.out.println("결과: " + result + "\n");
+                // 저장된 연산 결과 전부 출력
+                System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String input_result = sc.next();
+                if (input_result.trim().equals("inquiry")) {
+                    calc.inquiryResults();
+                }
 
-                // Getter 메서드 활용 - 연산 저장 결과 확인
-                System.out.println("연산 저장 결과: " + calc.getResultList() + "\n");
+                // 반복 여부 확인
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String input_continue = sc.next();
+                if (input_continue.trim().equals("exit")) { // 입력값이 "exit"면 반복 종료
+                    break;
+                }
+                System.out.println();
+            } else if (input == 2) {
+                while (true) {
+                    System.out.print("원의 반지름을 입력하세요: ");
+                    double radius = sc.nextInt();
+                    if (radius <= 0) { // 반지름은 음수가 될 수 없음
+                        System.out.println("원의 반지름은 양수만 입력 가능합니다.\n");
+                        continue; // 반복문으로 돌아가 다시 입력받기
+                    }
+                    else { // 원의 넓이 계산
+                        double result = calc.calculateCircleArea(radius);
+                        System.out.println("원의 넓이: " + result);
 
-            } catch (DivideByZeroException | InvalidOperatorException e) { // 콘솔에 예외 처리 메시지 출력할 수 있도록 처리
-                System.out.println("! 예외 발생: " + e.getMessage() + "\n");
-                continue; // 예외 발생 시 처음으로 다시 입력받기
+                        calc.inquiryCircleResults();
+                    }
+
+                    // 반복 여부 확인
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    String input_continue = sc.next();
+                    if (input_continue.trim().equals("exit")) { // 입력값이 "exit"면 반복 종료
+                        break;
+                    }
+                    System.out.println();
+                }
+                break; // 내부 while문 탈출
+            } else {
+                System.out.println("잘못된 입력입니다. 다시 입력해 주세요.");
             }
-
-            // Setter 활용 - 리스트 초기화 여부 확인
-            System.out.println("연산 결과가 저장된 리스트를 초기화하시겠습니까? (reset 입력 시 초기화)");
-            String input_reset = sc.next();
-            if (input_reset.trim().equals("reset")) {
-                calc.setResultList(new  ArrayList<>());
-                System.out.println("리스트가 초기화되었습니다." + calc.getResultList() + "\n");
-            }
-
-            // 가장 먼저 저장된 데이터 삭제 메서드
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String input_remove = sc.next();
-            if (input_remove.trim().equals("remove")) {
-                calc.removeResult();
-            }
-
-            // 저장된 연산 결과 전부 출력
-            System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String input_result = sc.next();
-            if (input_result.trim().equals("inquiry")) {
-                calc.inquiryResults();
-            }
-
-            // 반복 여부 확인
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String input_continue = sc.next();
-            if (input_continue.trim().equals("exit")) { // 입력값이 "exit"면 반복 종료
-                break;
-            }
-            System.out.println();
         }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,21 +4,38 @@ import java.util.*;
 
 public class Calculator {
     private List<Integer> resultList; // 연산 결과를 저장하는 리스트 생성
+    private List<Double> radiusList; // 원의 넓이 결괏값을 저장할 리스트 생성
 
-    // 생성자 구현
-    Calculator(List<Integer> paramList) {
-        this.resultList = new ArrayList<>(paramList);
+    // PI는 수학적으로 불변인 상수이기 때문에 final과 static 사용
+    final static double PI = 3.14159;
+
+    // 기본 생성자 구현
+    Calculator() {
+        this.resultList = new ArrayList<>(); // 사칙 연산 결과 저장 리스트 초기화
+        this.radiusList = new ArrayList<>(); // 원의 넓이 결괏값 저장 리스트 초기화
     }
 
-    // Getter 메서드 구현
+    // 사칙 연산 - Getter 메서드 구현
     public List<Integer> getResultList() {
         return new ArrayList<>(resultList); // 복사본을 반환하여 캡슐화 유지
     }
 
-    // Setter 메서드 구현
+    // 사칙 연산 - Setter 메서드 구현
     public void setResultList(List<Integer> newResultList) {
         this.resultList = new ArrayList<>(newResultList); // 리스트 복사본으로 안전한 수정이 가능하게 함
     }
+
+    // 원의 넓이 - Getter 메서드 구현
+    public List<Double> getRadiusList() {
+        return new ArrayList<>(this.radiusList);
+    }
+
+    // 원의 넓이 - Setter 메서드 구현
+    public void setRadiusList(List<Double> newRadiusList) {
+        this.radiusList = new ArrayList<>(newRadiusList); // 리스트 복사본으로 안전한 수정이 가능하게 함
+    }
+
+    // 생성자와 메서드 공간 구분
 
     public int calculate(int num1, int num2, char operator) throws DivideByZeroException, InvalidOperatorException {
         int result; // 결과 저장용 변수 선언
@@ -63,6 +80,22 @@ public class Calculator {
         System.out.print("저장된 연산 결과: ");
         for (int number : resultList) { // 향상된 for문 사용하여 연산 결과 출력
             System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+
+    // 원의 넓이를 계산하는 메서드 구현
+    public double calculateCircleArea(double radius) {
+        double result = radius * radius * PI; // 원의 넓이 계산
+        radiusList.add(result); // 결과 저장
+        return result; // 결괏값 반환
+    }
+
+    // 저장된 원의 넓이 결괏값을 조회하는 메서드 구현
+    public void inquiryCircleResults() {
+        System.out.print("저장된 원의 넓이 결과: ");
+        for (double number : radiusList) { // 향상된 for문 사용하여 연산 결과 출력
+            System.out.print(number + " | " + "\n");
         }
         System.out.println();
     }
